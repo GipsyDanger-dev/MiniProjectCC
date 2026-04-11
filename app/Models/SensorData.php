8d2d6a2bd3e636c1eeddeafa05d126c8a25070d7
sharkjira99@gmail.com <sharkjira99@gmail.com>
2026-04-11 18:34:07 +0700

    fix : add sensor api

--- app/Models/SensorData.php
+++ app/Models/SensorData.php
@@ -1,11 +1,24 @@
 <?php
+
 namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class SensorData extends Model
 {
-    protected $table = 'sensor_data'; 
+    use HasFactory;
+
+    // Tabel yang digunakan
+    protected $table = 'sensor_data';
+
+    // WAJIB DAFTARKAN SEMUA KOLOM DI SINI
     protected $fillable = [
-        'device_id', 'gas_value', 'smoke_value', 'temperature', 'flame_value', 'status_indikasi'
+        'device_id',
+        'gas_value',
+        'smoke_value',
+        'temperature',
+        'flame_value', // <--- INI YANG BIKIN DATA KAMU JADI 0 TADI
+        'status_indikasi'
     ];
 }
